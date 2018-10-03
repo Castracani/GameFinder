b75257bedd226b748150baf1990a8805d967d9ee
Castracani <willmyers993@gmail.com>
2018-10-03 11:55:10 -0400

    working survey page

---
+++ app/data/games.js
@@ -0,0 +1,9 @@
+let games = [
+  {
+    game: "Trails of Cold Steel",
+    image: "https://cdn.pastemagazine.com/www/articles/the%20legend%20of%20heroes%20trails%20of%20cold%20steel%20main.png",
+    scores: [
+      
+    ]
+  }
+]
