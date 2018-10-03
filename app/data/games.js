7ef2b23fc5f6e43757df8a56c94affe02b8b6c5f
Castracani <willmyers993@gmail.com>
2018-10-03 12:45:47 -0400

    further configuring survey form, added .json data

--- app/data/games.js
+++ app/data/games.js
@@ -3,7 +3,129 @@ let games = [
     game: "Trails of Cold Steel",
     image: "https://cdn.pastemagazine.com/www/articles/the%20legend%20of%20heroes%20trails%20of%20cold%20steel%20main.png",
     scores: [
-      
+      "1",
+      "5",
+      "3",
+      "1",
+      "4",
+      "1",
+      "4",
+      "1",
+      "1"
+    ]
+  },
+  {
+    game: "Dwarf Fortress",
+    image: "http://www.bay12games.com/dwarves/imgs/adv_camp_2.png",
+    scores: [
+      "2",
+      "3",
+      "1",
+      "1",
+      "4",
+      "1",
+      "5",
+      "5",
+      "1",
+      "2"
+    ]
+  },
+  {
+    game: "Fallout 76",
+    image: "https://gearnuke.b-cdn.net/wp-content/uploads/2018/07/fallout-76-featured-pc.jpg",
+    scores: [
+      "4",
+      "3",
+      "2",
+      "5",
+      "1",
+      "3",
+      "4",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    game: "Divinity: Original Sin II",
+    image: "https://i.kinja-img.com/gawker-media/image/upload/s--1HMmTKmz--/c_scale,f_auto,fl_progressive,q_80,w_800/rcgshof5iemau24fobyz.jpg",
+    scores: [
+      "3",
+      "5",
+      "4",
+      "1",
+      "4",
+      "2",
+      "3",
+      "3",
+      "2",
+      "4"
+    ]
+  },
+  {
+    game: "Portal",
+    image: "https://i.kym-cdn.com/entries/icons/original/000/000/252/thumb-050-portal-1.jpg",
+    scores: [
+      "2",
+      "2",
+      "5",
+      "3",
+      "5",
+      "4",
+      "4",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    game: "Stardew Valley",
+    image: "https://steamcdn-a.akamaihd.net/steam/apps/413150/ss_a3ddf22cda3bd722df77dbdd58dbec393906b654.1920x1080.jpg?t=1533135818",
+    scores: [
+      "2",
+      "3",
+      "3",
+      "3",
+      "4",
+      "5",
+      "4",
+      "2",
+      "1",
+      "1"
+    ]
+  },
+  {
+    game: "Fortnite",
+    image: "https://thumbor.forbes.com/thumbor/600x315/https%3A%2F%2Fblogs-images.forbes.com%2Finsertcoin%2Ffiles%2F2018%2F09%2Ffortnite-cube-new2.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "4",
+      "3",
+      "5"
+    ]
+  },
+  {
+    game: "Overwatch",
+    image: "https://cdn.mmos.com/wp-content/gallery/overwatch-overview-2/overwatch-featured-image.jpg",
+    scores: [
+      "5",
+      "2",
+      "5",
+      "1",
+      "1",
+      "1",
+      "4",
+      "2",
+      "5",
+      "5"
     ]
   }
-]
+];
+
+module.exports = games;
